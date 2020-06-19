10257abb3f072af035a4f32d015337ae4e88ba82
mavrickkc <mavrickkc@yahoo.com>
2020-06-19 16:56:41 +0000

    Lab 4

--- main.js
+++ main.js
@@ -27,15 +27,14 @@ const IPCIDR = require('ip-cidr');
 
 class IpAddress {
   constructor() {
-      // IAP's global log object is used to output errors, warnings, and other
+    // IAP's global log object is used to output errors, warnings, and other
     // information to the console, IAP's log files, or a Syslog server.
     // For more information, consult the Log Class guide on the Itential
     // Developer Hub https://developer.itential.io/ located
     // under Documentation -> Developer Guides -> Log Class Guide
     log.info('Starting the IpAddress product.');
   }
-
-/**
+  /**
  * Calculate and return the first host IP address from a CIDR subnet.
  * @param {string} cidrStr - The IPv4 subnet expressed
  *                 in CIDR format.
@@ -86,6 +85,7 @@ getFirstIpAddress(cidrStr, callback) {
   }
   return callback(object, callbackError);
 }
+
 }
 
 module.exports = new IpAddress;
